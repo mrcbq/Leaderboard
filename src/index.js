179777406a763ef9dbbdbb1a8a36b51398b251d0
mrcbq <marcobaquero@gmail.com>
2023-05-18 22:50:30 -0500

    add response for the API index.js file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,51 @@
 import './index.css';
+
+const gameId = '9pLtYc3QQCZyBxf3E3OD';
+const gameDataUrl = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores`;
+
+const inputName = document.getElementById('input-name');
+const inputScore = document.getElementById('input-score');
+const submitButton = document.getElementById('submit-button');
+const getScoresButton = document.getElementById('get-scores');
+
+submitButton.addEventListener('click', async (event) => {
+  event.preventDefault();
+
+  const formData = {
+    user: inputName.value,
+    score: parseInt(inputScore.value, 10),
+  };
+
+  try {
+    const response = await fetch(gameDataUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    });
+    const responseData = await response.json();
+    console.log(responseData);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+getScoresButton.addEventListener('click', async () => {
+  try {
+    const response = await fetch(gameDataUrl);
+    const responseData = await response.json();
+
+    const scoresContainer = document.getElementById('scores-container');
+    scoresContainer.innerHTML = '';
+
+    const scoresArray = Object.values(responseData.result);
+    scoresArray.forEach((scoreData) => {
+      const li = document.createElement('li');
+      li.textContent = `${scoreData.user}: ${scoreData.score}`;
+      scoresContainer.appendChild(li);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+});
